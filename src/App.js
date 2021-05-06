a91d86791ed28062a2dbb9a34933975d32d982c8
Sergio Obolevich <sergioobolevich@gmail.com>
2021-05-06 17:51:04 -0300

    Clean create-react-app unused files and code.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-2xl italic">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p className="text-8xl italic text-center mt-10">Hello there</p>
       </header>
     </div>
   );
