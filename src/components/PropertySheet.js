0a126860d9ff7e0294ecd5ca9af44caa22088b44
Sergio Obolevich <sergioobolevich@gmail.com>
2021-05-13 23:39:21 -0300

    Set property sheet component layout.

--- src/components/PropertySheet.js
+++ src/components/PropertySheet.js
@@ -6,18 +6,30 @@ const PropertySheet = () => {
   const propertyId = parseInt(useParams().id, 10);
   const allProperties = useSelector((state) => state.properties);
 
-  const productInfo = allProperties.filter((p) => p.id === propertyId)[0];
+  const propertyInfo = allProperties.filter((p) => p.id === propertyId)[0];
 
   return (
-    <main className="text-center">
+    <main>
       <header className="bg-gray-300 py-5">
-        <h1 className="text-5xl italic">
+        <h1 className="text-2xl pl-5 md:text-5xl italic">
           Property Sheet for
-          {` ${productInfo.title}`}
+          {` ${propertyInfo.title}`}
         </h1>
       </header>
       <div className="flex flex-wrap p-10">
-        { `useParams property ID: ${propertyId}` }
+        <div className="w-full md:w-1/2">
+          <img src={propertyInfo.img_url} alt={propertyInfo.title} />
+        </div>
+        <div className="mt-4 lg:mt-0 lg:pl-5">
+          <h2 className="text-2xl font-black">{propertyInfo.title}</h2>
+          <p>{`Address: ${propertyInfo.address}`}</p>
+          <p className="mt-4 italic font-black">Description:</p>
+          <p className="mb-4 italic">{propertyInfo.desc}</p>
+          <p>{`Sqr Meters: ${propertyInfo.sqrmeters}`}</p>
+          <p>{`Bedrooms: ${propertyInfo.bedrooms}`}</p>
+          <p>{`Bathrooms: ${propertyInfo.baths}`}</p>
+          <p className="text-green-700 font-black text-xl mt-5">{`Price: $${propertyInfo.price}`}</p>
+        </div>
       </div>
     </main>
   );
